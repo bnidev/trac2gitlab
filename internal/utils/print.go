f56eb04e7f0b27c3a8b0cdc211570e4ecbfa803c
Benjamin Diener <bd@benjamindiener.com>
2025-07-29 22:29:39 +0200

    feat(utils): add formatted message support to print functions and new PrintInfo

--- internal/utils/print.go
+++ internal/utils/print.go
@@ -1,42 +1,52 @@
 package utils
 
 import (
-    "fmt"
+	"fmt"
 
-    "github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss"
 )
 
 // printStyled is an unexported helper function that prints a styled icon and message
 // with the specified color code. Used internally by the public print functions.
 func printStyled(icon string, colorCode string, msg string) {
-    iconStyled := lipgloss.NewStyle().
-        Bold(true).
-        Foreground(lipgloss.Color(colorCode)).
-        Render(icon)
+	iconStyled := lipgloss.NewStyle().
+		Bold(true).
+		Foreground(lipgloss.Color(colorCode)).
+		Render(icon)
 
-    textStyled := lipgloss.NewStyle().
-        Foreground(lipgloss.Color(colorCode)).
-        Render(msg)
+	textStyled := lipgloss.NewStyle().
+		Foreground(lipgloss.Color(colorCode)).
+		Render(msg)
 
-    fmt.Println(iconStyled, textStyled)
+	fmt.Println(iconStyled, textStyled)
 }
 
 // PrintSuccess prints a success message with a checkmark icon.
-func PrintSuccess(msg string) {
-    printStyled("✓", "10", msg)
+func PrintSuccess(format string, args ...any) {
+	msg := fmt.Sprintf(format, args...)
+	printStyled("✓", "10", msg)
 }
 
 // PrintError prints an error message with an 'x' icon.
-func PrintError(msg string) {
-    printStyled("x", "9", msg)
+func PrintError(format string, args ...any) {
+	msg := fmt.Sprintf(format, args...)
+	printStyled("x", "9", msg)
 }
 
 // PrintWarning prints a warning message with an '!' icon.
-func PrintWarning(msg string) {
-    printStyled("!", "11", msg)
+func PrintWarning(format string, args ...any) {
+	msg := fmt.Sprintf(format, args...)
+	printStyled("!", "11", msg)
 }
 
 // PrintInProgress prints a message indicating an ongoing process with a '>' icon.
-func PrintInProgress(msg string) {
-    printStyled(">", "14", msg)
+func PrintInProgress(format string, args ...any) {
+	msg := fmt.Sprintf(format, args...)
+	printStyled(">", "14", msg)
+}
+
+// PrintInfo prints an informational message with an 'i' icon.
+func PrintInfo(format string, args ...any) {
+	msg := fmt.Sprintf(format, args...)
+	printStyled("i", "12", msg)
 }
