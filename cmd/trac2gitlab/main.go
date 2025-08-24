7056c77a80bb1a33e6f7b2335fc659aac870817b
Benjamin Diener <bd@benjamindiener.com>
2025-08-24 19:21:58 +0200

    refactor(main): create AppContext and load configuration with log level option

--- cmd/trac2gitlab/main.go
+++ cmd/trac2gitlab/main.go
@@ -3,12 +3,30 @@ package main
 import (
 	"log/slog"
 
+	"github.com/bnidev/trac2gitlab/internal/app"
 	"github.com/bnidev/trac2gitlab/internal/cli"
+	"github.com/bnidev/trac2gitlab/internal/config"
 	"github.com/bnidev/trac2gitlab/internal/utils"
 )
 
 func main() {
-	cli.Execute()
 	logger := utils.NewLogger("")
 	slog.SetDefault(logger.Logger)
+
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		slog.Error("Failed to load configuration", "errorMsg", err)
+		return
+	}
+
+	if cfg.General.LogLevel != "" {
+		logger.SetLevel(cfg.General.LogLevel)
+	}
+
+	ctx := &app.AppContext{
+		Config: &cfg,
+		Logger: logger.Logger,
+	}
+
+	cli.Execute(ctx)
 }
